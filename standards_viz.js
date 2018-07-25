782bbc84c797bb9b0b91ae8de6b94239b07895c5
Emily Dolson <EmilyLDolson@gmail.com>
2018-07-26 08:32:33 +0900

    Working version

---
+++ standards_viz.js
@@ -0,0 +1,79 @@
+var data = [];
+
+function change_color() {
+    var color_col = document.getElementById("color_col").value;
+    console.log(color_col);
+
+    if (color_col == "") {
+        color_col="ID";
+    }
+
+    var max = d3.max(data, function(d) { return d[color_col]; } );
+
+    var color_scale = d3.scaleSequential(d3.interpolateViridis).domain([0, max]);
+    d3.selectAll("circle").style("fill", function(d){return color_scale(d.data.data[color_col])});
+}
+
+function make_viz(filename) {
+    var svg = d3.select("#viz");
+    data = d3.csvParse(filename);
+    data.push({"ID":-1, "ancestors":"[null]"}); 
+    // console.log(data);
+
+
+    var strat_data = d3.stratify()
+    .id(function(d) { return d.ID; })
+    .parentId(function(d) { var a = JSON.parse(d.ancestors); return a[0]; })
+    (data);
+    // console.log(strat_data);
+    var root = d3.hierarchy(strat_data);
+    var treeLayout = d3.tree();
+    treeLayout.size([800, 1500]);
+    treeLayout(root);
+
+    // Nodes
+    svg.selectAll('circle')
+    .data(root.descendants())
+    .enter()
+    .append('circle')
+    .classed('node', true)
+    .attr('cy', function(d) {return d.x;})
+    .attr('cx', function(d) {return d.y;})
+    .attr('r', 4);
+
+    change_color();
+
+    // Links
+    svg.selectAll('line')
+    .data(root.links())
+    .enter()
+    .append('line')
+    .classed('link', true)
+    .style("stroke", "black")
+    .attr('y1', function(d) {return d.source.x;})
+    .attr('x1', function(d) {return d.source.y;})
+    .attr('y2', function(d) {return d.target.x;})
+    .attr('x2', function(d) {return d.target.y;});      
+}
+
+// handle upload button
+// from http://bl.ocks.org/syntagmatic/raw/3299303/
+function upload_button(el, callback) {
+    var uploader = document.getElementById(el);  
+    var reader = new FileReader();
+  
+    reader.onload = function(e) {
+      var contents = e.target.result;
+      callback(contents);
+    };
+  
+    uploader.addEventListener("change", handleFiles, false);  
+  
+    function handleFiles() {
+      d3.select("#table").text("loading...");
+      var file = this.files[0];
+      reader.readAsText(file);
+    };
+  };
+
+  
